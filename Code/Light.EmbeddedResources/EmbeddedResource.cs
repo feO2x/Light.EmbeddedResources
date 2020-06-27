aa0593d23a1913b94db9fa9f4e34b99d052a207f
Kenny Pflug <kenny.pflug@live.de>
2020-06-27 13:32:22 +0200

    finished csproj, added XML comments for v1.0.0 release

--- Code/Light.EmbeddedResources/EmbeddedResource.cs
+++ Code/Light.EmbeddedResources/EmbeddedResource.cs
@@ -5,28 +5,67 @@ using Light.GuardClauses;
 
 namespace Light.EmbeddedResources
 {
+    /// <summary>
+    /// Provides extensions methods to easily access embedded resources.
+    /// </summary>
     public static class EmbeddedResource
     {
-        public static Stream GetEmbeddedStream(this Type typeInSameNamespace, string resourceName) =>
-            GetEmbeddedStreamInternal(typeInSameNamespace.MustNotBeNull(nameof(typeInSameNamespace)), resourceName);
+        /// <summary>
+        /// Gets a stream to an embedded resource. The name is assembled from the namespace of the specified type and the resource name.
+        /// </summary>
+        /// <param name="typeInSameNamespace">A type that resides in the same assembly and namespace as the target embedded resource.</param>
+        /// <param name="name">The case-sensitive name of the resource.</param>
+        /// <exception cref="EmbeddedResourceNotFoundException">Thrown when the specified resource cannot be found.</exception>
+        /// <exception cref="ArgumentNullException">Thrown when any parameter is null.</exception>
+        /// <exception cref="ArgumentException">Thrown when <paramref name="name"/> is an empty string.</exception>
+        public static Stream GetEmbeddedStream(this Type typeInSameNamespace, string name) =>
+            GetEmbeddedStreamInternal(typeInSameNamespace.MustNotBeNull(nameof(typeInSameNamespace)), name);
 
-        public static Stream GetEmbeddedStream<T>(this T instanceInSameNamespace, string resourceName) =>
-            GetEmbeddedStreamInternal(instanceInSameNamespace.MustNotBeNullReference(nameof(instanceInSameNamespace))!.GetType(), resourceName);
+        /// <summary>
+        /// Gets a stream to an embedded resource. The name is assembled from the namespace of the specified instance and the resource name.
+        /// </summary>
+        /// <param name="instanceInSameNamespace">An instance whose type resides in the same assembly and namespace as the target embedded resource.</param>
+        /// <param name="name">The case-sensitive name of the resource.</param>
+        /// <exception cref="EmbeddedResourceNotFoundException">Thrown when the specified resource cannot be found.</exception>
+        /// <exception cref="ArgumentNullException">Thrown when any parameter is null.</exception>
+        /// <exception cref="ArgumentException">Thrown when <paramref name="name"/> is an empty string.</exception>
+        public static Stream GetEmbeddedStream<T>(this T instanceInSameNamespace, string name) =>
+            GetEmbeddedStreamInternal(instanceInSameNamespace.MustNotBeNullReference(nameof(instanceInSameNamespace))!.GetType(), name);
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static Stream GetEmbeddedStreamInternal(Type typeInSameNamespace, string resourceName)
+        private static Stream GetEmbeddedStreamInternal(Type typeInSameNamespace, string name)
         {
-            var stream = typeInSameNamespace.Assembly.GetManifestResourceStream(typeInSameNamespace, resourceName);
+            var stream = typeInSameNamespace.Assembly.GetManifestResourceStream(typeInSameNamespace, name);
             if (stream == null)
-                throw new EmbeddedResourceNotFoundException(nameof(resourceName), $"There is no resource called \"{typeInSameNamespace.Namespace + "." + resourceName}\" in assembly \"{typeInSameNamespace.Assembly}\".");
+                throw new EmbeddedResourceNotFoundException(nameof(name), $"There is no resource called \"{typeInSameNamespace.Namespace + "." + name}\" in assembly \"{typeInSameNamespace.Assembly}\".");
             return stream;
         }
 
-        public static void CopyEmbeddedStreamToFile(this Type typeInSameNameSpace, string resourceName, string filePath) =>
-            CopyEmbeddedStreamToFileInternal(typeInSameNameSpace.MustNotBeNull(nameof(typeInSameNameSpace)), resourceName, filePath);
+        /// <summary>
+        /// Copies an embedded resource stream to a file. The name is assembled from the namespace of the specified type and the resource name.
+        /// </summary>
+        /// <param name="typeInSameNamespace">A type that resides in the same assembly and namespace as the target embedded resource.</param>
+        /// <param name="name">The case-sensitive name of the resource.</param>
+        /// <param name="filePath">The relative or absolute path for the file that will be written to.</param>
+        /// <exception cref="EmbeddedResourceNotFoundException">Thrown when the specified resource cannot be found.</exception>
+        /// <exception cref="IOException">Thrown when an I/O error occured during copying.</exception>
+        /// <exception cref="ArgumentNullException">Thrown when any parameter is null.</exception>
+        /// <exception cref="ArgumentException">Thrown when <paramref name="name"/> is an empty string.</exception>
+        public static void CopyEmbeddedStreamToFile(this Type typeInSameNamespace, string name, string filePath) =>
+            CopyEmbeddedStreamToFileInternal(typeInSameNamespace.MustNotBeNull(nameof(typeInSameNamespace)), name, filePath);
 
-        public static void CopyEmbeddedStreamToFile<T>(this T instanceInSameNameSpace, string resourceName, string filePath) =>
-            CopyEmbeddedStreamToFileInternal(instanceInSameNameSpace.MustNotBeNullReference(nameof(instanceInSameNameSpace))!.GetType(), resourceName, filePath);
+        /// <summary>
+        /// Copies an embedded resource stream to a file. The name is assembled from the namespace of the specified type and the resource name.
+        /// </summary>
+        /// <param name="instanceInSameNamespace">An instance whose type resides in the same assembly and namespace as the target embedded resource.</param>
+        /// <param name="name">The case-sensitive name of the resource.</param>
+        /// <param name="filePath">The relative or absolute path for the file that will be written to.</param>
+        /// <exception cref="EmbeddedResourceNotFoundException">Thrown when the specified resource cannot be found.</exception>
+        /// <exception cref="IOException">Thrown when an I/O error occured during copying.</exception>
+        /// <exception cref="ArgumentNullException">Thrown when any parameter is null.</exception>
+        /// <exception cref="ArgumentException">Thrown when <paramref name="name"/> is an empty string.</exception>
+        public static void CopyEmbeddedStreamToFile<T>(this T instanceInSameNamespace, string name, string filePath) =>
+            CopyEmbeddedStreamToFileInternal(instanceInSameNamespace.MustNotBeNullReference(nameof(instanceInSameNamespace))!.GetType(), name, filePath);
 
         private static void CopyEmbeddedStreamToFileInternal(Type typeInSameNameSpace, string resourceName, string filePath)
         {
--- Code/Light.EmbeddedResources/EmbeddedResourceNotFoundException.cs
+++ Code/Light.EmbeddedResources/EmbeddedResourceNotFoundException.cs
@@ -3,11 +3,22 @@ using System.Runtime.Serialization;
 
 namespace Light.EmbeddedResources
 {
+    /// <summary>
+    /// Indicates that an embedded resource could not be found.
+    /// </summary>
     [Serializable]
     public class EmbeddedResourceNotFoundException : ArgumentException
     {
+        /// <summary>
+        /// Initializes a new instance of <see cref="EmbeddedResourceNotFoundException"/>.
+        /// </summary>
+        /// <param name="parameterName">The name of the parameter (optional).</param>
+        /// <param name="message">The message of the exception (optional).</param>
         public EmbeddedResourceNotFoundException(string? parameterName = null, string? message = null) : base(message, parameterName) { }
 
+        /// <summary>
+        /// This constructor is used for serialization purposes.
+        /// </summary>
         protected EmbeddedResourceNotFoundException(SerializationInfo info, StreamingContext context): base(info, context) { }
     }
 }
